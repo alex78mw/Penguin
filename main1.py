cec08d150856f8b41c60b0b53d73aeec02ca67a9
alex78mw <alex_mawas@yahoo.fr>
2016-05-26 22:52:07 +0200

    Adding left/right functions

--- main1.py
+++ main1.py
@@ -96,4 +96,227 @@ game = {
     'enter':0,
     'exit':0,
     'moves': []
-}
+
+def leftKey(event):
+    global currentLvl
+    global playerMode
+    global game
+    penguin = game["pos"]
+    if game["moves"][penguin-1] != 'W':
+        if game["moves"][penguin-1] == 'S':
+            game["score"] += 1
+            game["moves"][penguin] = 'B'
+            game["pos"] -= 1
+            game["moves"][penguin-1] = 'P'
+            printGame()
+            canvas.delete(canvas.find_withtag("result"))
+            if playerMode == 1:
+                if currentLvl == 1:
+                    canvas.create_text(800,150,text="You Win ! Level 2 now !", tags="result")
+                    currentLvl = 2
+                    init()
+                    win(0)
+                elif currentLvl == 2:
+                    canvas.create_text(800,150,text="You Win ! Level 3 now !", tags="result")
+                    currentLvl = 3
+                    init()
+                    win(1)
+                else:
+                    canvas.create_text(800,150,text="You Win ! Congratulations !", tags="result")
+                    win(2)
+                printGame()
+            else:
+                if currentLvl == 1:
+                    if game["score"] == 13:
+                        canvas.create_text(800,150,text="You Win ! Level 2 now !", tags="result")
+                        currentLvl = 2
+                        init()
+                        win(0)
+                    else:
+                        canvas.create_text(800,150,text="You Loose... Read above...", tags="result")
+                        retry(1)
+                elif currentLvl == 2:
+                    if game["score"] == 43:
+                        canvas.create_text(800,150,text="You Win ! Level 3 now !", tags="result")
+                        currentLvl = 3
+                        init()
+                        win(1)
+                    else:
+                        canvas.create_text(800,150,text="You Loose... Read above...", tags="result")
+                        retry(1)
+                else:
+                    if game["score"] == 77:
+                        canvas.create_text(800,150,text="You Win ! Congratulations !", tags="result")
+                        win(2)
+                    else:
+                        canvas.create_text(800,150,text="You Loose... Read above...", tags="result")
+                        retry(1)
+                printGame()
+        elif game["moves"][penguin-1] == 'B':
+            if game["life"] == 1:
+                if currentLvl == 1:
+                    game = game1
+                elif currentLvl == 2:
+                    game = game2
+                else:
+                    game = game3
+                game["life"] = 0
+                printGame()
+            else:
+                canvas.delete(canvas.find_withtag("result"))
+                canvas.create_text(800,150,text="You Loose...", tags="result")
+                retry(0)
+        elif game["moves"][penguin-1] == 'C':
+            game["score"] +=3
+            game["moves"][penguin] = 'B'
+            game["pos"] -= 1
+            game["moves"][penguin-1] = 'P'
+            printGame()
+        elif game["moves"][penguin-1] == 'H':
+            game["hammerPower"] = 1
+            game["score"] +=1
+            game["moves"][penguin] = 'B'
+            game["pos"] -= 1
+            game["moves"][penguin-1] = 'P'
+            printGame()
+        elif game["moves"][penguin-1] == 'G':
+            game["life"] = 1
+            game["score"] +=1
+            game["moves"][penguin] = 'B'
+            game["pos"] -= 1
+            game["moves"][penguin-1] = 'P'
+            printGame()
+        elif game["moves"][penguin-1] == 'T':
+            game["score"] +=1
+            game["moves"][penguin] = 'B'
+            game["pos"] = game3["exit"]
+            game["moves"][game3["exit"]] = 'P'
+            printGame()
+        else:
+            game["score"] +=1
+            game["moves"][penguin] = 'B'
+            game["pos"] -= 1
+            game["moves"][penguin-1] = 'P'
+            printGame()
+    else:
+        if game["hammerPower"] == 1:
+            game["score"] +=1
+            game["moves"][penguin] = 'B'
+            game["pos"] -= 1
+            game["moves"][penguin-1] = 'P'
+            game["hammerPower"] = 0
+            printGame() 
+        else:
+            print("hammerPower : 0")        
+
+def rightKey(event):
+    global currentLvl
+    global playerMode
+    global game
+    penguin = game["pos"]
+    if game["moves"][penguin+1] != 'W':
+        if game["moves"][penguin+1] == 'S':
+            game["score"] += 1
+            game["moves"][penguin] = 'B'
+            game["pos"] += 1
+            game["moves"][penguin+1] = 'P'
+            printGame()
+            canvas.delete(canvas.find_withtag("result"))
+            if playerMode == 1:
+                if currentLvl == 1:
+                    canvas.create_text(800,150,text="You Win ! Level 2 now !", tags="result")
+                    currentLvl = 2
+                    init()
+                    win(0)
+                elif currentLvl == 2:
+                    canvas.create_text(800,150,text="You Win ! Level 3 now !", tags="result")
+                    currentLvl = 3
+                    init()
+                    win(1)
+                else:
+                    canvas.create_text(800,150,text="You Win ! Congratulations !", tags="result")
+                    win(2)
+                printGame()
+            else:
+                if currentLvl == 1:
+                    if game["score"] == 13:
+                        canvas.create_text(800,150,text="You Win ! Level 2 now !", tags="result")
+                        currentLvl = 2
+                        init()
+                        win(0)
+                    else:
+                        canvas.create_text(800,150,text="You Loose... Read above...", tags="result")
+                        retry(1)
+                elif currentLvl == 2:
+                    if game["score"] == 43:
+                        canvas.create_text(800,150,text="You Win ! Level 3 now !", tags="result")
+                        currentLvl = 3
+                        init()
+                        win(1)
+                    else:
+                        canvas.create_text(800,150,text="You Loose... Read above...", tags="result")
+                        retry(1)
+                else:
+                    if game["score"] == 77:
+                        canvas.create_text(800,150,text="You Win ! Congratulations !", tags="result")
+                        win(2)
+                    else:
+                        canvas.create_text(800,150,text="You Loose... Read above...", tags="result")
+                        retry(1)
+                printGame()
+        elif game["moves"][penguin+1] == 'B':
+            if game["life"] == 1:
+                if currentLvl == 1:
+                    game = game1
+                elif currentLvl == 2:
+                    game = game2
+                else:
+                    game = game3
+                game["life"] = 0
+                printGame()
+            else:
+                canvas.delete(canvas.find_withtag("result"))
+                canvas.create_text(800,150,text="You Loose...", tags="result")
+                retry(0)
+        elif game["moves"][penguin+1] == 'C':
+            game["score"] +=3
+            game["moves"][penguin] = 'B'
+            game["pos"] += 1
+            game["moves"][penguin+1] = 'P'
+            printGame()
+        elif game["moves"][penguin+1] == 'H':
+            game["hammerPower"] = 1
+            game["score"] +=1
+            game["moves"][penguin] = 'B'
+            game["pos"] += 1
+            game["moves"][penguin+1] = 'P'
+            printGame()
+        elif game["moves"][penguin+1] == 'G':
+            game["life"] = 1
+            game["score"] +=1
+            game["moves"][penguin] = 'B'
+            game["pos"] += 1
+            game["moves"][penguin+1] = 'P'
+            printGame()
+        elif game["moves"][penguin+1] == 'T':
+            game["score"] +=1
+            game["moves"][penguin] = 'B'
+            game["pos"] = game3["exit"]
+            game["moves"][game3["exit"]] = 'P'
+            printGame()
+        else:
+            game["score"] +=1
+            game["moves"][penguin] = 'B'
+            game["pos"] += 1
+            game["moves"][penguin+1] = 'P'
+            printGame()
+    else:
+        if game["hammerPower"] == 1:
+            game["score"] +=1
+            game["moves"][penguin] = 'B'
+            game["pos"] += 1
+            game["moves"][penguin+1] = 'P'
+            game["hammerPower"] = 0
+            printGame() 
+        else:
+            print("hammerPower : 0")}
